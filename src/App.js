ca72bed1bdb510d4654fe066671ad4f59511cb63
Wasek <wesinem@gmail.com>
2020-07-19 23:46:44 -0700

    refactored game

--- src/App.js
+++ src/App.js
@@ -1,66 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { calculateWinner } from "./helper";
+import Board from "./components/Board";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      board: ['', '', '', '', '', '', '', '', ''],
-      player: 'X'
-    }
-  }
+const App = () => {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [stepNumber, setStepNumber] = useState(0);
+    const [xIsNext, setXisNext] = useState(true);
+    const winner = calculateWinner(history[stepNumber]);
+    const xO = xIsNext ? "X" : "O";
 
-  squareClicked(index){
-    let player = this.state.player;
-    let board = this.state.board;
+    const handleClick = (i) => {
+        const historyPoint = history.slice(0, stepNumber + 1);
+        const current = historyPoint[stepNumber];
+        const squares = [...current];
+        // return if won or occupied
+        if (winner || squares[i]) return;
+        // select square
+        squares[i] = xO;
+        setHistory([...historyPoint, squares]);
+        setStepNumber(historyPoint.length);
+        setXisNext(!xIsNext);
+    };
 
-    board[index] = player;
-    let winning_combos =[
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6],
-    ]
+    const jumpTo = (step) => {
+        setStepNumber(step);
+        setXisNext(step % 2 === 0);
+    };
 
-    for (let i=0; i<winning_combos.length;i++){
-      let winning_row = winning_combos[i]
-      let p1= winning_row[0];
-      let p2= winning_row[1];
-      let p3= winning_row[2];
+    const renderMoves = () =>
+        history.map((_step, move) => {
+            const destination = move ? `Go to move #${move}` : "Go to Start";
+            return (
+                <li key={move}>
+                    <button onClick={() => jumpTo(move)}>{destination}</button>
+                </li>
+            );
+        });
 
-      if (board[p1] == board[p2] && board[p2] == board[p3] && board[p3] == board[p1]){
-        alert(`Player ${player} has won the game`);
-      }
-    }
-    console.log(board);
-
-    player = (player == 'X')? 'O' : 'X';
-
-    this.setState({
-      player: player,
-      board: board
-    })
-
-    console.log(index);
-  }
-  render() {
     return (
-      <div className="App">
-        <h1>Tic Tac Toe</h1>
-        <div className='board'>
-          {this.state.board.map((square, index) => {
-            return <div className='square' key = {index} onClick={()=> this.squareClicked(index)}>{square}</div>
-          })}
-        </div>
-      </div>
+        <>
+            <h1>React Tic Tac Toe - With Hooks</h1>
+            <Board squares={history[stepNumber]} onClick={handleClick} />
+            <div className="info-wrapper">
+                <div>
+                    <h3>History</h3>
+                    {renderMoves()}
+                </div>
+                <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
+            </div>
+        </>
     );
-  }
-}
-
+};
 
 export default App;
